1155b0bf64d511ac294c3e88f027cb5549c74d7b
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-15 15:20:34 +0600

    tailwind configuration

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Login = (props) => {
-    return (
-        <div className="login-container">
-            <h1 className="welcome-message">Welcome to decentralized voting application</h1>
-            <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
-        </div>
-    )
-}
+  return (
+    <div className="login-container">
+      <h1 className="welcome-message">Decentralized Voting System</h1>
+      <button className="login-button" onClick={props.connectWallet}>
+        Connect Metamask
+      </button>
+    </div>
+  );
+};
 
 export default Login;
